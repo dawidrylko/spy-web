5b59fe65127d1e47bcd2b1d8bc218ef0934e93a7
Dawid Ryłko <dawid.rylko@gmail.com>
2017-08-21 19:46:32 +0200

    Test ssl redirection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const sslRedirect = require('heroku-ssl-redirect');
 const app = express();
+
+app.use(sslRedirect());
 
 app.use(express.static(__dirname));
 app.listen(process.env.PORT || 8080);
